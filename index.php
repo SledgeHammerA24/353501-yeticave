cb3cb17e7ab032af3ee5d7c2b6a46ca824b8b35e
SledgeHammerA24 <Mrdarcy2003@mail.ru>
2017-09-03 10:38:35 +0400

    Добавил массивы и обход массивов

--- index.php
+++ index.php
@@ -22,6 +22,50 @@ $hours_delta_time=floor(($tomorrow-$now)/3600); // вычисляем часы
 $minutes_delta_time=floor(($tomorrow-$now)%3600 / 60); // вычисляем минуты
 $lot_time_remaining=sprintf("%02d:%02d", $hours_delta_time, $minutes_delta_time); // переопределяем переменную $lot_time_remaining
 
+// Организация простого массива - список категорий товаров
+$categories_array = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+// Организация ассоциативного массива - лоты (6 шт) в котором содержится список свойств лота - Название, Категория, Цена, URL Картинки
+$item_card_1 = [
+  'Название' => '2014 Rossignol District Snowboard',
+  'Категория' => 'Доски и лыжи',
+  'Цена' => 10999,
+  'URL Картинки' => 'img/lot-1.jpg'
+];
+$item_card_2 = [
+  'Название' => 'DC Ply Mens 2016/2017 Snowboard',
+  'Категория' => 'Доски и лыжи',
+  'Цена' => 159999,
+  'URL Картинки' => 'img/lot-2.jpg'
+];
+$item_card_3 = [
+  'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+  'Категория' => 'Крепления',
+  'Цена' => 8000,
+  'URL Картинки' => 'img/lot-3.jpg'
+];
+$item_card_4 = [
+  'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
+  'Категория' => 'Ботинки',
+  'Цена' => 10999,
+  'URL Картинки' => 'img/lot-4.jpg'
+];
+$item_card_5 = [
+  'Название' => 'Куртка для сноуборда DC Mutiny Charocal',
+  'Категория' => 'Одежда',
+  'Цена' => 7500,
+  'URL Картинки' => 'img/lot-5.jpg'
+];
+$item_card_6 = [
+  'Название' => 'Маска Oakley Canopy',
+  'Категория' => 'Разное',
+  'Цена' => 5400,
+  'URL Картинки' => 'img/lot-6.jpg'
+];
+
+// Организация двумерного массива - лоты (6 шт) уложены в простой индексированный массив
+$items_array = [$item_card_1, $item_card_2, $item_card_3, $item_card_4, $item_card_5, $item_card_6];
+
 
 ?>
 <!DOCTYPE html>
@@ -101,34 +145,40 @@ $lot_time_remaining=sprintf("%02d:%02d", $hours_delta_time, $minutes_delta_time)
             <h2>Открытые лоты</h2>
             <select class="lots__select">
                 <option>Все категории</option>
-                <option>Доски и лыжи</option>
-                <option>Крепления</option>
-                <option>Ботинки</option>
-                <option>Одежда</option>
-                <option>Инструменты</option>
-                <option>Разное</option>
+                <?php foreach ($categories_array as $value) : ?>
+                  <option><?=$value;?></option>
+                <?php endforeach ?>
             </select>
         </div>
+
         <ul class="lots__list">
-            <li class="lots__item lot">
+          <?php foreach ($items_array as $key => $value) : ?>
+              <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="img/lot-1.jpg" width="350" height="260" alt="Сноуборд">
+                    <img src="<?=$value['URL Картинки'];?>" width="350" height="260" alt="Сноуборд">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category">Доски и лыжи</span>
-                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a></h3>
+                    <span class="lot__category"><?=$value['Категория'];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="lot.html"><?=$value['Название'];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">10 999<b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$value['Цена'];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                           <?=$lot_time_remaining;?>
+
                         </div>
                     </div>
                 </div>
             </li>
+          <?php endforeach ?>
         </ul>
+
+
+
+
+
     </section>
 </main>
 
